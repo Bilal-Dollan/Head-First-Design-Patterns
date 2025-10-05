8f5730d7cad55d8f429ddb37d6036d36d13b6d3d
Bilal-Dollan <bilaldollan199@gmail.com>
2025-10-05 23:48:04 +0300

    WatherStation first stage complete

---
+++ WeatherStation/WeatherData.java
@@ -0,0 +1,40 @@
+package WeatherStation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeatherData implements Subject  {
+    private List<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;    
+
+    public WeatherData(){
+        observers = new ArrayList<Observer>();
+    }
+
+    public void registerObserver(Observer o){
+        observers.add(o);
+    }
+
+    public void removeObserver(Observer o){
+        observers.remove(o);
+    }
+
+    public void notifyObservers(){
+        for (Observer observer: observers){
+            observer.update(temperature, humidity, pressure);
+        }
+    }
+
+    public void measurementsChanged(){
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temp, float humidity, float pressure){
+        this.humidity = humidity;
+        this.temperature = temp;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+}
